139883c6d6df6c65ab2a0a82b30e6e7fab21404e
Gabriel Rodriguez <gabrielrdz@gmail.com>
2011-04-22 20:39:40 -0500

    Add first method

--- Helpers.cs
+++ Helpers.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 
@@ -7,5 +9,17 @@ namespace GR7
 {
     public static class Helpers
     {
+        public static IList<string> LlenarComboNombresMes(CultureInfo culture)
+        {
+            var list = new List<string>();
+
+            for (int i = 1; i <= 12; i++)
+            {
+                list.Add(culture.TextInfo.ToTitleCase(culture.DateTimeFormat.GetMonthName(i)));
+            }
+
+            return list;
+        }
     }
+
 }
